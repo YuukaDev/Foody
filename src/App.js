f0d0035b16f8b9049eb50354165f98539e363e5a
YuukaDev <vule.amv1@gmail.com>
2021-12-17 00:56:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // Components
 import Navigation from "./components/Navigation/Navigation";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div>
       <Navigation />
+      <Main />
     </div>
   );
 }
