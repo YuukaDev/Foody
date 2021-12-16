a61a2a895daa1f4745d9dafcb32b014909ecdea0
YuukaDev <vule.amv1@gmail.com>
2021-12-17 00:55:57 +0100

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,35 +1,42 @@
 import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+import { Nav, Navbar, Container } from "react-bootstrap";
 
 function Navigation() {
-  const [open, isOpen] = useState(true);
-
   return (
-    <nav className="navigation-wrapper">
-      <div className="navigation-container">
-        <div className="logo-wrapper">
-          <a href="../App.js">
-            <h1>Hello</h1>
-          </a>
-        </div>
-        <ul className="navigation-items">
-          <li>
-            <a href="#about">Home</a>
-          </li>
-          <li>
-            <a href="#projects">About</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-          <li>
-            <a href="#" className="sign-in">
-              <button>Sign In</button>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#474554",
+        fontFamily: "Ubuntu",
+      }}
+    >
+      <Navbar className="p-3">
+        <Container>
+          <Nav style={{ gap: "10px", fontSize: "1.5em" , color: "#ACA7CB" }}>
+            <Nav.Link href="#about" style={{ transition: "0.5s all ease" }}>
+              About
+            </Nav.Link>
+            <Nav.Link href="#contact" style={{ transition: "0.5s all ease" }}>
+              Recipes
+            </Nav.Link>
+            <Nav.Link className="foodie">
+              Foody
+            </Nav.Link>
+            <Nav.Link href="#contact" style={{ transition: "0.5s all ease" }}>
+              Contact
+            </Nav.Link>
+            <Nav.Link style={{ transition: "0.5s all ease" }}>Sign In</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
 
 export default Navigation;
+
+//font-family: 'Puppies Play', cursive;
