cbfe77b2c958c4819b16fb674a656082fee0a0d7
YuukaDev <vule.amv1@gmail.com>
2021-12-19 01:56:35 +0100

    Create About.js

---
+++ src/components/About/About.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="paragraph-section" id="about">
+      <div className="title">
+        <h1>About</h1>
+        <h1>Foody</h1>
+      </div>
+      <hr />
+      <p className="paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+        pulvinar mattis diam, nec eleifend lacus maximus ut. Cras rutrum tortor
+        ac gravida lobortis. Cras tristique cursus orci id euismod. Donec
+        gravida dolor mauris, a semper eros sodales aliquam. Phasellus eget
+        libero tincidunt, dignissim dui id, vehicula arcu. Sed id luctus mi, in
+        sagittis dolor. Mauris faucibus volutpat feugiat. Phasellus tristique
+        orci lorem, ac interdum arcu auctor non. Mauris aliquam, mauris a
+        posuere pellentesque, lectus lectus dictum velit, non pellentesque nunc
+        augue ut felis. Curabitur auctor, justo sed venenatis facilisis, massa
+        ipsum mattis elit, nec commodo tellus enim a est. Phasellus a dignissim
+        ligula. Vestibulum vel justo ipsum. Mauris commodo lectus elit, a
+        vulputate libero rutrum sed.
+      </p>
+      <p className="paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+        pulvinar mattis diam, nec eleifend lacus maximus ut. Cras rutrum tortor
+        ac gravida lobortis. Cras tristique cursus orci id euismod. Donec
+        gravida dolor mauris, a semper eros sodales aliquam. Phasellus eget
+        libero tincidunt, dignissim dui id, vehicula arcu. Sed id luctus mi, in
+        sagittis dolor. Mauris faucibus volutpat feugiat. Phasellus tristique
+        orci lorem, ac interdum arcu auctor non. Mauris aliquam, mauris a
+        posuere pellentesque, lectus lectus dictum velit, non pellentesque nunc
+        augue ut felis. Curabitur auctor, justo sed venenatis facilisis, massa
+        ipsum mattis elit, nec commodo tellus enim a est. Phasellus a dignissim
+        ligula. Vestibulum vel justo ipsum. Mauris commodo lectus elit, a
+        vulputate libero rutrum sed.
+      </p>
+    </div>
+  );
+}
+
+export default About;
