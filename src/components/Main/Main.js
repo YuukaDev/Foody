d1f76ed6cc0a8682e6fa56bcc424f52f685c7f57
YuukaDev <vule.amv1@gmail.com>
2021-12-19 01:05:36 +0100

    updated

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,15 @@
 import React from "react";
-
 function Main() {
-  return <div className="main"></div>;
+  return (
+    <div className="hero-wrapper">
+      <div className="img-container"></div>
+      <div className="main" id="about">
+        <h1>Fast.</h1>
+        <h1>Beautiful.</h1>
+        <h1>Recipes.</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
