52e0af008a6c216a9f2f66a3d08e660b31e613e7
YuukaDev <vule.amv1@gmail.com>
2021-12-19 01:56:41 +0100

    updated

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -13,16 +13,16 @@ function Navigation() {
         <nav>
           <ul className="nav__links">
             <li>
-              <a href="#">Home</a>
+              <a href="#home">Home</a>
             </li>
             <li>
-              <a href="#">About</a>
+              <a href="#about">About</a>
             </li>
             <li>
-              <a href="#">Recipes</a>
+              <a href="#recipes">Recipes</a>
             </li>
             <li>
-              <a href="#">Contact</a>
+              <a href="#contact">Contact</a>
             </li>
           </ul>
         </nav>
