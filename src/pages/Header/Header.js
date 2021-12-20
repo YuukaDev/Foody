9583cdb637f9fba24076433d64c70b465a59c89a
YuukaDev <vule.amv1@gmail.com>
2021-12-20 22:08:42 +0100

    Create Header.js

---
+++ src/pages/Header/Header.js
@@ -0,0 +1,13 @@
+import Navigation from "../../components/Navigation/Navigation";
+import Main from "../../components/Main/Main";
+
+function Header() {
+  return (
+    <div>
+      <Navigation />
+      <Main />
+    </div>
+  );
+}
+
+export default Header;
