8c1a7ef628ae56d444b9e6cabba08cadabdb84de
YuukaDev <vule.amv1@gmail.com>
2021-12-20 22:00:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@
 import Navigation from "./components/Navigation/Navigation";
 import Main from "./components/Main/Main";
 import About from "./components/About/About";
+import Recipes from "./components/Recipes/Recipes";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Navigation />
-      <Main />
-      <About />
-    </div>
+    <>
+      <div>
+        <Navigation />
+        <Main />
+        <About />
+        <Routes>
+          <Route path="/recipes" element={<Recipes />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
