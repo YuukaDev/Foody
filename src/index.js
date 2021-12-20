11a1331464d9b0f09e88d7b5a2cddd937b1246a5
YuukaDev <vule.amv1@gmail.com>
2021-12-20 22:00:38 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from "react-dom";
 import "./style/style.css";
 import App from "./App";
+import React from "react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
