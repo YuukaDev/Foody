113a6ef4d45d53571a7cf0065edeba845006c317
YuukaDev <vule.amv1@gmail.com>
2021-12-21 22:48:59 +0100

    Update ContentRecipes.js

--- src/components/ContentRecipes/ContentRecipes.js
+++ src/components/ContentRecipes/ContentRecipes.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Navigation from "../Navigation/Navigation";
 
+import { Bounce } from "react-reveal";
+
 function ContentRecipes() {
   return (
     <div className="recipes-wrapper">
       <Navigation />
-      <div className="main">
-        <h1>Search</h1>
-        <h1>Your</h1>
-        <h1>Recipes.</h1>
+      <div className="recipes-hero">
+        <Bounce left>
+          <h1>Search</h1>
+          <h1>Beautiful</h1>
+          <h1>Recipe.</h1>
+        </Bounce>
       </div>
       <div className="content-container"></div>
     </div>
