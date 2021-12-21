41af886990cf6aef4f3a532e8036482ae9218782
YuukaDev <vule.amv1@gmail.com>
2021-12-21 22:21:46 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ReactDOM from "react-dom";
-import "./style/style.css";
-import App from "./App";
 import React from "react";
+import ReactDOM from "react-dom";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./style/style.css";
+
+import App from "./App";
+import ContentRecipes from "./components/ContentRecipes/ContentRecipes";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/recipes" element={<ContentRecipes />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById("root")
 );
