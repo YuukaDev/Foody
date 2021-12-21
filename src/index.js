4af96b845967e9fad9e5c7c932f2efdaee6be122
YuukaDev <vule.amv1@gmail.com>
2021-12-21 23:11:42 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import "./style/style.css";
 
 import App from "./App";
 import ContentRecipes from "./components/ContentRecipes/ContentRecipes";
+import Test from "./Test";
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/recipes" element={<ContentRecipes />} />
+      <Route path="/testing" element={<Test />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
