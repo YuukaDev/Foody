7b3d7699901aea6a4a27339fb01efb327422576d
YuukaDev <vule.amv1@gmail.com>
2021-12-21 22:21:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,33 @@
 import Header from "./pages/Header/Header";
 import Main from "./pages/Main/Main";
 
-import Recipes from "./components/Recipes/Recipes";
+//import Recipes from "./components/Recipes/Recipes";
 
 import { Routes, Route } from "react-router-dom";
+import React, { StrictMode } from "react";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Main />
-      <Routes>
-        <Route path="/recipes" element={<Recipes />} />
-      </Routes>
-    </div>
+    <>
+      <div>
+        <Header />
+        <Main />
+      </div>
+      <StrictMode>
+        <Routes>
+          <Route path="/recipes" element={<Recipes />} />
+        </Routes>
+      </StrictMode>
+    </>
   );
 }
 
 export default App;
+
+function Recipes() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
