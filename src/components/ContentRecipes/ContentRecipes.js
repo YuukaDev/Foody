e782204a1bec1339190d50f507fa2a920334cd2e
YuukaDev <vule.amv1@gmail.com>
2021-12-21 22:21:43 +0100

    Create ContentRecipes.js

---
+++ src/components/ContentRecipes/ContentRecipes.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function ContentRecipes() {
+  return (
+    <div className="recipes">
+      <p>
+        Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello
+        WorldHello WorldHello WorldHello World
+      </p>
+      <p>
+        Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello
+        WorldHello WorldHello WorldHello World
+      </p>
+      <p>
+        Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello
+        WorldHello WorldHello WorldHello World
+      </p>
+      <p>
+        Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello
+        WorldHello WorldHello WorldHello World
+      </p>
+      <p>
+        Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello
+        WorldHello WorldHello WorldHello World
+      </p>
+      <p>
+        Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello
+        WorldHello WorldHello WorldHello World
+      </p>
+    </div>
+  );
+}
+
+export default ContentRecipes;
