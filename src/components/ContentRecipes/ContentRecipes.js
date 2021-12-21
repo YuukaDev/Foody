372e8425d8eaeb01f680d23e3e089a6b714006d5
YuukaDev <vule.amv1@gmail.com>
2021-12-21 22:26:06 +0100

    Update ContentRecipes.js

--- src/components/ContentRecipes/ContentRecipes.js
+++ src/components/ContentRecipes/ContentRecipes.js
@@ -1,32 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Navigation from "../Navigation/Navigation";
 
 function ContentRecipes() {
   return (
-    <div className="recipes">
-      <p>
-        Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello
-        WorldHello WorldHello WorldHello World
-      </p>
-      <p>
-        Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello
-        WorldHello WorldHello WorldHello World
-      </p>
-      <p>
-        Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello
-        WorldHello WorldHello WorldHello World
-      </p>
-      <p>
-        Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello
-        WorldHello WorldHello WorldHello World
-      </p>
-      <p>
-        Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello
-        WorldHello WorldHello WorldHello World
-      </p>
-      <p>
-        Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello
-        WorldHello WorldHello WorldHello World
-      </p>
+    <div className="recipes-wrapper">
+      <Navigation />
+      <div className="main">
+        <h1>Search</h1>
+        <h1>Your</h1>
+        <h1>Recipes.</h1>
+      </div>
+      <div className="content-container"></div>
     </div>
   );
 }
