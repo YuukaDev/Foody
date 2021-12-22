b6a98129e1ee67376623680e036e099054fa2d30
YuukaDev <vule.amv1@gmail.com>
2021-12-23 00:31:56 +0100

    Update ContentRecipes.js

--- src/components/ContentRecipes/ContentRecipes.js
+++ src/components/ContentRecipes/ContentRecipes.js
@@ -4,6 +4,13 @@ import Navigation from "../Navigation/Navigation";
 import { Bounce } from "react-reveal";
 
 function ContentRecipes() {
+  const searchRecipes = async (query) => {
+    let res = await fetch(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=48749e6b&app_key=%20e48b4d118c5df082474141e6e4746f1a`
+    );
+    let random = await res.json();
+    console.log(random.hits[0]);
+  };
   return (
     <div className="recipes-wrapper">
       <Navigation />
