80326c7f463f337ba5c3efb9c8da97a00be4b790
YuukaDev <vule.amv1@gmail.com>
2021-12-22 23:26:06 +0100

    Update ContentRecipes.js

--- src/components/ContentRecipes/ContentRecipes.js
+++ src/components/ContentRecipes/ContentRecipes.js
@@ -1,29 +1,28 @@
 import React, { useState, useEffect } from "react";
 import Navigation from "../Navigation/Navigation";
 
-import Logo from "../assets/images/domino.png";
-
 import { Bounce } from "react-reveal";
 
 function ContentRecipes() {
   return (
     <div className="recipes-wrapper">
       <Navigation />
-      <div className="recipes-hero">
-        <Bounce left>
+      <Bounce left>
+        <div className="recipes-hero">
           <h1 className="hover-text">Foody Recipes</h1>
-        </Bounce>
-      </div>
-      <div className="form-container">
-        <form>
-          <input
-            type="text"
-            id="recipesInput"
-            placeholder="Enter a username..."
-          />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+        </div>
+        <div className="form-container">
+          <form>
+            <input
+              type="text"
+              autoComplete="off"
+              id="recipesInput"
+              placeholder="Search for recipe..."
+            />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </Bounce>
     </div>
   );
 }
