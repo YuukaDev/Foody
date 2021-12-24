150b52b8736d1dbafbb331bddbda818c98dfd188
YuukaDev <vule.amv1@gmail.com>
2021-12-25 00:23:04 +0100

    Update SignUp.js

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -30,11 +30,11 @@ function SignUp() {
         className="d-flex justify-content-center align-items-center"
       >
         <Card
-          style={{ width: "28rem", backgroundColor: "#3a65da" }}
+          style={{ width: "28rem", backgroundColor: "#2a4d69" }}
           className="p-5"
         >
           <Card.Body>
-            <h2 className="text-light">Sign Up</h2>
+            <h2 className="fs-1 text-light">Sign Up</h2>
           </Card.Body>
           <Form>
             <Form.Group
@@ -43,10 +43,15 @@ function SignUp() {
               }}
               className="p-2"
             >
-              <Form.Label className="text-light" htmlFor="email">
+              <Form.Label className="fs-4 text-light" htmlFor="email">
                 Email
               </Form.Label>
-              <Form.Control id="email" type="email" required />
+              <Form.Control
+                autoComplete="off"
+                id="email"
+                type="email"
+                required
+              />
             </Form.Group>
             <Form.Group
               onChange={(e) => {
@@ -54,26 +59,38 @@ function SignUp() {
               }}
               className="p-2"
             >
-              <Form.Label className="text-light" htmlFor="password">
+              <Form.Label className="fs-4 text-light" htmlFor="password">
                 Password
               </Form.Label>
-              <Form.Control id="password" type="password" required />
+              <Form.Control
+                autoComplete="off"
+                id="password"
+                type="password"
+                required
+              />
             </Form.Group>
             <Button
               style={{
-                backgroundColor: "#67e1ff",
+                backgroundColor: "#4b86b4",
                 outline: "none",
                 border: "none",
+                width: "100%",
               }}
               onClick={register}
-              className="w-50 mt-3"
+              className="mt-3"
               type="button"
             >
               Sign Up
             </Button>
           </Form>
           <div className="mt-3 text-center text-light">
-            Already have an account? Login
+            Already have an account?{" "}
+            <a
+              style={{ color: "#e7eff6", textDecoration: "none" }}
+              href="/login"
+            >
+              Login
+            </a>
           </div>
         </Card>
       </Container>
