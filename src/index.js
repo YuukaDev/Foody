4668ce73245345583ed8c66f9352784ba877a287
YuukaDev <vule.amv1@gmail.com>
2021-12-29 22:05:42 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./style/style.css";
 
 import App from "./App";
-import Recipes from "./pages/Recipes/Recipes";
-import SignUp from "./components/SignUp/SignUp";
-import Login from "./components/Login/Login";
+import About from "./components/About/About";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/recipes" element={<Recipes />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<App />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
