5b759555b7d40b1121007842a08d821b63cf89b8
YuukaDev <vule.amv1@gmail.com>
2021-12-29 22:05:08 +0100

    Create About.jsx

---
+++ src/components/About/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Box, Heading, Text } from "@chakra-ui/react";
+
+function About() {
+  return (
+    <>
+      <Container>
+        <Box>
+            <Heading>
+                Test
+            </Heading>
+            <Text>
+                Test
+            </Text>
+        </Box>
+      </Container>
+    </>
+  );
+}
+
+export default About;
