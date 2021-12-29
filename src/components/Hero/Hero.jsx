f4e8e82b6852dfdad5eed3df7bcb3ac062f08714
YuukaDev <vule.amv1@gmail.com>
2021-12-29 22:05:23 +0100

    Create Hero.jsx

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Heading, Text, HStack, Box } from "@chakra-ui/react";
+
+function Hero() {
+  return (
+    <>
+      <HStack
+        textAlign="center"
+        justifyContent="center"
+        alignItems="center"
+        mt="100px"
+      >
+        <Box>
+          <Heading fontSize="4em" size="lg">
+            Foody
+          </Heading>
+          <Text size="md" mt="10px" fontSize="1.5em">
+            Search for fast and beautfiul recipes
+          </Text>
+        </Box>
+      </HStack>
+    </>
+  );
+}
+
+export default Hero;
