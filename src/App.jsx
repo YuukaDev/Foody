d5b9a46ee4f5c72afa0141b558d41fc8c3d5e15e
YuukaDev <vule.amv1@gmail.com>
2022-01-02 21:09:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { ChakraProvider, ThemeProvider, theme } from "@chakra-ui/react";
 import Navigation from "./components/Navigation/Navigation";
 import Hero from "./components/Hero/Hero";
 import ContentRecipes from "./components/ContentRecipes/ContentRecipes";
+import Footer from "./components/Footer/Footer";
 import About from "./components/About/About";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Navigation />
           <Hero />
           <ContentRecipes />
+          <Footer />
         </ChakraProvider>
       </ThemeProvider>
       <StrictMode>
