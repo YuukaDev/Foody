c140a436031b4252fc960a26a389cc7f743e8fa9
YuukaDev <vule.amv1@gmail.com>
2022-01-02 19:54:54 +0100

    Update ContentRecipes.jsx

--- src/components/ContentRecipes/ContentRecipes.jsx
+++ src/components/ContentRecipes/ContentRecipes.jsx
@@ -18,7 +18,7 @@ import { Fade } from "react-reveal";
 import Testing from "../Testing/Testing";
 
 function ContentRecipes() {
-  const { onOpen } = useDisclosure();
+  const { onOpen, isOpen, onClose } = useDisclosure();
   const [recipes, setRecipes] = useState([]);
   const getRecipe = async (query) => {
     try {
@@ -65,55 +65,57 @@ function ContentRecipes() {
             templateColumns="repeat(3, 1fr)"
           >
             {recipes.map((recipe, index) => (
-              <Fade left>
-                <GridItem
-                  key={index}
-                  onClick={onOpen}
-                  style={{
-                    width: "300px",
-                    height: "300px",
-                    marginTop: "50px",
-                  }}
-                  boxShadow="dark-lg"
-                >
-                  <Image
-                    borderRadius="24px"
+              <>
+                <Fade left>
+                  <GridItem
+                    key={index}
                     style={{
-                      heigth: "450px",
-                      cursor: "pointer",
-                      overflow: "hidden",
-                      transition: "0.5s all ease",
+                      width: "300px",
+                      height: "300px",
+                      marginTop: "50px",
                     }}
-                    src={recipe.recipe.image}
-                  />
-                  <Box
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                    color="#fff"
-                    borderRadius="6px"
-                    height="40px"
-                    position="relative"
-                    top="2"
-                    width="100%"
-                    bg="teal.900"
+                    boxShadow="dark-lg"
                   >
-                    <Heading
-                      mb="20px"
-                      textAlign="center"
-                      fontSize="1.5em"
-                      mt="20px"
-                    >
-                      {recipe.recipe.label}
-                    </Heading>
-                    <Testing
-                      heading={recipe.recipe.label}
-                      cautions={recipe.recipe.cautions}
-                      ingredient={recipe.recipe.ingredientLines}
+                    <Image
+                      borderRadius="24px"
+                      onClick={() => {
+                        <Testing
+                          heading={recipe.recipe.label}
+                          cautions={recipe.recipe.cautions}
+                          ingredient={recipe.recipe.ingredientLines}
+                        />;
+                      }}
+                      style={{
+                        heigth: "450px",
+                        overflow: "hidden",
+                        transition: "0.5s all ease",
+                      }}
+                      src={recipe.recipe.image}
                     />
-                  </Box>
-                </GridItem>
-              </Fade>
+                    <Box
+                      display="flex"
+                      justifyContent="center"
+                      alignItems="center"
+                      color="#fff"
+                      borderRadius="6px"
+                      height="40px"
+                      position="relative"
+                      top="2"
+                      width="100%"
+                      bg="teal.900"
+                    >
+                      <Heading
+                        mb="20px"
+                        textAlign="center"
+                        fontSize="1.5em"
+                        mt="20px"
+                      >
+                        {recipe.recipe.label}
+                      </Heading>
+                    </Box>
+                  </GridItem>
+                </Fade>
+              </>
             ))}
           </Grid>
         </Container>
