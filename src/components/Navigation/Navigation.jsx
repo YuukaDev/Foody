efde3cff9e4411d3c98d461ba5672d7f7c0866fb
YuukaDev <vule.amv1@gmail.com>
2022-01-03 21:34:06 +0100

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,58 +1,58 @@
-import "./Navigation.css";
 import { HStack, Heading, Button, Link } from "@chakra-ui/react";
 import DarkMode from "../DarkMode/DarkMode";
 
 const Navigation = () => {
   return (
-    <HStack
-    className="navigation-wrapper"
-      mt="15px"
-      padding="25%"
-      justifyContent="space-between"
-      alignItems="center"
-      py={5}
-    >
-      <Link
-        textDecoration="none"
-        _hover={{
-          textDecoration: "none",
-        }}
+    <div className="navigation-container">
+      <HStack
+        mt="15px"
+        padding="25%"
+        justifyContent="space-between"
+        alignItems="center"
+        py={5}
       >
-        <Heading size="lg">Foody.</Heading>
-      </Link>
-      <HStack alignItems="center" spacing={{ base: 0, md: 2 }}>
         <Link
-          transition="0.5s all ease"
           textDecoration="none"
-          href="/about"
           _hover={{
-            transition: "0.5s all ease",
-            transform: "scale(1.1)",
             textDecoration: "none",
           }}
         >
-          <Button size="md" variant="ghost">
-            About
-          </Button>
+          <Heading size="lg">Foody.</Heading>
         </Link>
-        <Link
-          transition="0.5s all ease"
-          textDecoration="none"
-          _hover={{
-            transition: "0.5s all ease",
-            transform: "scale(1.1)",
-            textDecoration: "none",
-          }}
-        >
-          <Button size="md" variant="ghost">
-            GitHub
-          </Button>
-        </Link>
-        <Link>
-          <DarkMode />
-        </Link>
+        <HStack alignItems="center" spacing={{ base: 0, md: 2 }}>
+          <Link
+            transition="0.5s all ease"
+            textDecoration="none"
+            href="/about"
+            _hover={{
+              transition: "0.5s all ease",
+              transform: "scale(1.1)",
+              textDecoration: "none",
+            }}
+          >
+            <Button size="md" variant="ghost">
+              About
+            </Button>
+          </Link>
+          <Link
+            transition="0.5s all ease"
+            textDecoration="none"
+            _hover={{
+              transition: "0.5s all ease",
+              transform: "scale(1.1)",
+              textDecoration: "none",
+            }}
+          >
+            <Button size="md" variant="ghost">
+              GitHub
+            </Button>
+          </Link>
+          <Link>
+            <DarkMode />
+          </Link>
+        </HStack>
       </HStack>
-    </HStack>
+    </div>
   );
 };
 
