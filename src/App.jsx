aec5b382953a08e0c7374c88dbedf22e3d7db100
YuukaDev <vule.amv1@gmail.com>
2022-01-03 22:28:08 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
           <Navigation />
           <Hero />
           <ContentRecipes />
+          <Footer />
         </ChakraProvider>
       </ThemeProvider>
       <StrictMode>
